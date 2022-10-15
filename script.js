6f1b6b18473ff08f810a53fa377269209e27a6a7
Pablo Espasandin <101610017+PabloEspasa@users.noreply.github.com>
2022-10-14 23:18:24 -0300

    Update script.js
    Se realizaron correcciones

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+//Creo la clase del objeto producto con su respectivo contrusctor
+
 class Producto {
     constructor(nombre, precio, tipo) {
         this.nombre  = nombre;
-        this.precio  = parseFloat(precio);
+        this.precio  = parseFloat(precio).toFixed(2);
         this.tipo=tipo;
     }    
 }
 
 
 
-let continuar;
-const productos = [];
+let continuar;//defino la variable que me mantiene dentro del bucle
+
+const productos = [];//defino el array donde quedaran guardados los datos de cada uno de los productos que ingrese
+
+//Creamos la función para cargar los items dentro del array
+
+function CargarCarrito(){
+
+    let carrito = new Producto();
+
 do {    
     
     productos.push(new Producto(prompt("Ingrese nombre del producto"),prompt("Ingrese precio del producto"),prompt("Ingrese tipo")));
@@ -17,9 +27,14 @@ do {
 
 }while (continuar == "si");
 
+return carrito;
+}
 
+//Llamamos a la funcion
+CargarCarrito();
 
+//recorro el array
+for (let carrito of productos);
 
-for (const producto of productos);
-
-console.log(productos);
+//mostramos en consola
+console.log(productos); 
